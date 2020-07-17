f23c345f53a014d63af7a49263a3d805b5e60c8d
Javier Pereyra <javier.pereyra@gmail.com>
2020-07-17 01:48:23 -0300

    mas consultas - producto: 1

---
+++ scripts/consultas/producto.sql
@@ -0,0 +1,22 @@
+-- Consulta para contar cantidad de productos
+-- select count(P.ID)
+-- from Producto as P
+
+-- Listar productos 
+select P.descripcion
+from Producto as P
+
+-- Listar Stock y productos
+select P.descripcion, P.Stock
+from Producto as P
+
+-- Listar producto con Mayor stock
+-- TODO
+
+-- Listar producto con Menor stock
+-- TODO
+
+-- Stock de un producto
+-- select P.Stock
+-- from Producto as P
+-- where P.Descripcion = 'pantalon'
