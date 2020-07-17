8b6d73977302395c86e5436b9c9ac4c48fb0f323
Javier Pereyra <javier.pereyra@gmail.com>
2020-07-16 21:24:34 -0300

    consultas

---
+++ scripts/consultas/cantidad_ventas.sql
@@ -0,0 +1,11 @@
+-- Script para contar cantidad de ventas
+-- select Count(*)
+-- from ventas;
+
+select *
+from ventas
+
+
+-- Script sumar el total de todas las ventas
+-- select sum(V.ValorTotaldeVenta)
+-- from Ventas as V
