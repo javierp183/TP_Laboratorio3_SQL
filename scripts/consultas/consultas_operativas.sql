0ed669367951e7da0b1b4c5e0e570b3d35f5f7c8
Javier Pereyra <javier.pereyra@gmail.com>
2020-07-17 01:42:46 -0300

    mas consultas - consultas_operativas: 10

--- scripts/consultas/consultas_operativas.sql
+++ scripts/consultas/consultas_operativas.sql
@@ -9,3 +9,8 @@
 -- select C.Nombres, C.Apellidos, C.Email, C.Sexo, C.FechaNac, C.CUIL
 -- from Cliente as C
 -- where C.CUIL = '20333'
+
+-- Consulta para obtener datos de stock por descripcion
+-- select P.Descripcion, P.Color, P.Precio, P.Stock
+-- from Producto as P
+-- where P.Descripcion = 'pantalon'
