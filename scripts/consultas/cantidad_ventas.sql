ac8165b09b4395562f7ba3e533d9ea5346297220
Javier Pereyra <javier.pereyra@gmail.com>
2020-07-16 21:35:50 -0300

    mas consultas - ventas: 1

--- scripts/consultas/cantidad_ventas.sql
+++ scripts/consultas/cantidad_ventas.sql
@@ -9,3 +9,16 @@ from ventas
 -- Script sumar el total de todas las ventas
 -- select sum(V.ValorTotaldeVenta)
 -- from Ventas as V
+
+-- Script para contar la cantidad de ventas por Operador
+-- select count(O.Usuario)
+-- from ventas as V
+--     inner join Operador as O on O.ID = V.IDOperador
+-- where O.Usuario = 'javier'
+
+
+-- Script para sumar la cantidad de ventas por Operador de Productos
+-- select sum(V.cantidadTotalProductos)
+-- from ventas as V
+--     inner join Operador as O on O.ID = V.IDOperador
+-- where O.Usuario = 'javier'
