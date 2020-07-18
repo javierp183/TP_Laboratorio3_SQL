60d6f4e972be390ac62744f45026dbc307bb019d
Javier Pereyra <javier.pereyra@gmail.com>
2020-07-17 22:05:37 -0300

    subconsultas: venta total por cada pais

--- scripts/consultas/pais.sql
+++ scripts/consultas/pais.sql
@@ -8,4 +8,12 @@ select P.Nombre,
 from Pais as P
 
 
--- Listar suma total vendida por cada País.- ( subconsulta ): TODO
+-- Listar suma total vendida por cada País.- ( subconsulta ):
+select P.Nombre,
+    (
+    select sum(V.ValorTotaldeVenta)
+    from Operador as O
+        inner join Ventas as V on V.IDOperador = O.ID
+    where P.ID = O.IDPais
+) as [Venta total por Pais]
+from Pais as P
