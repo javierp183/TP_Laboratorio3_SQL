d39ec3eebe53c90ade03bb284f88f71d9543747a
Javier Pereyra <javier.pereyra@gmail.com>
2020-07-17 22:01:06 -0300

    subconsultas: cliente por cada pais

--- scripts/consultas/consultas_operativas.sql
+++ scripts/consultas/consultas_operativas.sql
@@ -1,16 +1,19 @@
--- Consulta para obtener datos de un usuario para la aplicacion.
+-- Consulta para obtener datos de un usuario para la aplicacion
+-- apartir de su UserID
 -- select O.Usuario, O.Nombres, O.Apellidos, O.Clave
 -- from Operador as O
 --     inner join Pais as P on P.ID = O.IDPais
 -- where O.Usuario = 'javier'
 
 -- Consulta para obtener datos de un cliente para la aplicacion
+-- apartir de numero de CUIL - numero unico e identificatorio
 -- por número de CUIL.
 -- select C.Nombres, C.Apellidos, C.Email, C.Sexo, C.FechaNac, C.CUIL
 -- from Cliente as C
 -- where C.CUIL = '20333'
 
--- Consulta para obtener datos de stock por descripcion
+-- Consulta para obtener datos de stock por nombre de producto 
+-- Descripcion
 -- select P.Descripcion, P.Color, P.Precio, P.Stock
 -- from Producto as P
 -- where P.Descripcion = 'pantalon'
--- scripts/consultas/pais.sql
+++ scripts/consultas/pais.sql
@@ -1,2 +1,11 @@
--- Listar cantidad de clientes por cada País.- ( subconsulta ): TODO
+-- Listar cantidad de clientes por cada País.- ( subconsulta ):
+select P.Nombre,
+    (
+        select count(C.ID)
+    from Cliente as C
+    where C.IDPais = P.ID
+) as [Cantidad de Clientes]
+from Pais as P
+
+
 -- Listar suma total vendida por cada País.- ( subconsulta ): TODO
