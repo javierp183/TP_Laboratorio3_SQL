fde72d98e2aa19290fefdca91af64858decf7b28
Javier Pereyra <javier.pereyra@gmail.com>
2020-07-18 17:57:51 -0300

    Edades medias en vistas

--- scripts/vistas/cliente.sql
+++ scripts/vistas/cliente.sql
@@ -15,3 +15,41 @@
 -- -- Reporte de la vista
 -- select *
 -- from VW_ReporteDeOperadores
+
+-- crear y listar clientes con su nombre y apellido y CUIL.-
+-- create view VW_ReporteDeClientes
+-- as
+--     select C.Nombres, C.Apellidos, C.Email, C.Sexo, C.CUIL
+--     from Cliente as C
+
+-- drop view VW_ReporteDeClientes
+
+-- select *
+-- from VW_ReporteDeClientes
+
+-- -- View para listar la edad media de los clientes
+
+-- create view VW_EdadMediaClientes
+-- as
+--     select avg(cast(datediff(day,C.FechaNac,getdate())/365.25 as Int)) as [Media de Edad]
+--     from Cliente as C
+
+-- drop view VW_EdadMediaClientes
+
+-- select *
+-- from VW_EdadMediaClientes
+
+-- -- View para listar los clientes que superen la edad media
+-- Dependencia con la vista VW_EdadMediaClientes
+-- create view VW_ClientesQueSuperanEdadMedia
+-- as
+--     select C.nombres, C.Apellidos, C.Email
+--     from Cliente as C
+--     where cast(datediff(day,C.FechaNac,getdate())/365.25 as Int) > (
+--         select *
+--     from VW_EdadMediaClientes
+--     )
+
+-- Listar los Clientes que superan la edad Media
+-- select *
+-- from VW_ClientesQueSuperanEdadMedia
