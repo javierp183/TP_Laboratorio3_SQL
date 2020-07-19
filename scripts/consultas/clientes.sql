7a4566cf07a77d1dd0a477aa63efd8dbc8e34d14
Javier Pereyra <javier.pereyra@gmail.com>
2020-07-19 11:42:50 -0300

    DER update

--- scripts/consultas/clientes.sql
+++ scripts/consultas/clientes.sql
@@ -1,6 +1,6 @@
 -- Script para listar la edad y nombre  y apellido de los clientes
--- select C.Nombres, C.Apellidos, cast(datediff(day,C.FechaNac,getdate())/365.25 as Int) as Edad
--- from Cliente as C
+select C.Nombres, C.Apellidos, cast(datediff(day,C.FechaNac,getdate())/365.25 as Int) as Edad
+from Cliente as C
 
 -- -- Script para listar la edad media de los clientes
 -- select avg(cast(datediff(day,C.FechaNac,getdate())/365.25 as Int)) as [Media de Edad]
