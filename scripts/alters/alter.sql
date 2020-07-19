8a847ef7c913571b053719695dd3ec4a5db1e376
Javier Pereyra <javier.pereyra@gmail.com>
2020-07-19 19:04:52 -0300

    misc alter

---
+++ scripts/alters/alter.sql
@@ -0,0 +1,13 @@
+-- use sventaropa
+-- Agrego columna
+-- alter table Cliente add Alta bigint
+
+-- Agrego una Primary Key
+-- alter table Cliente add CONSTRAINT PK_Alta Primary KEY (Alta)
+
+-- Drop de tabla
+-- drop table Cliente
+
+-- Drop the Primary Key
+-- ALTER TABLE Cliente
+-- DROP CONSTRAINT PK_Alta; 
